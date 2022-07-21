62ca1e50987a05e7fec22aa44980d031cdd15976
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-21 13:50:46 -0500

    79. Test listProducts endpoint. Closes #18

--- src/test/java/da/springframework/springbootwebfluxapirest/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/da/springframework/springbootwebfluxapirest/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,29 @@
 package da.springframework.springbootwebfluxapirest;
 
+import da.springframework.springbootwebfluxapirest.model.documents.Product;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    void testListProducts() {
+
+        webTestClient.get()
+                .uri("/api/v2/products")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Product.class)
+                .hasSize(9);
     }
 
 }
