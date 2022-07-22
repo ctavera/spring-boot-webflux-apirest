425076b7f26b0bd52676ff0a8f31ae8e4b246c71
christian.tavera <christian.tavera@grupoargos.com.mx>
2022-07-22 12:07:58 -0500

    86. Executing test on Mock mode. Closes #25

--- src/test/java/da/springframework/springbootwebfluxapirest/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/da/springframework/springbootwebfluxapirest/SpringBootWebfluxApirestApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
@@ -18,7 +19,9 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //netty server mode
+@AutoConfigureWebTestClient // mock server mode
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // mock server mode
 class SpringBootWebfluxApirestApplicationTests {
 
     @Autowired
